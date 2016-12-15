5a7e0434071235bc3e74fd7f6e31217391533e32
Luo Chunhui <wo@luochunhui.com>
2016-12-15 14:24:44 +0800

    jen

--- src/com/jfpal/Remote.groovy
+++ src/com/jfpal/Remote.groovy
@@ -28,7 +28,7 @@ class Remote implements Serializable {
     }
 
     def deploy( Map conf ) {
-      deploy conf.playbook conf.inventory, conf.file, conf.get("tags", [])
+      deploy( conf.playbook, conf.inventory, conf.file, conf.get("tags", []) )
     }
 
     def deploy( String playbook, String inventory, String file, String[] tags=[]) {
