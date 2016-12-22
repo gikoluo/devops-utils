f176f393eaa04dc64d835470409b7d25a37e3eab
Luo Chunhui <wo@luochunhui.com>
2016-12-22 10:48:23 +0800

    API

--- src/jenkinsfile/dsbbc/Api.groovy
+++ src/jenkinsfile/dsbbc/Api.groovy
@@ -1,20 +1,21 @@
 #!groovy 
 
 @Library('jfpal')
+
 import com.jfpal.lib.Utilities
 import com.jfpal.lib.Remote
 
 def utils = new Utilities(steps)
 def playbook = "dsbbc/bbcapi"
-def buildProjectName = "builds/Backend"
+def buildProjectName = "builds/API"
+def targetFile = 'jifu-platform-server/target/jifu-platform-server-1.0.1-SNAPSHOT.jar'
+def archivePublisher = true
 
-targetFile = "jifu-platform-server/target/jifu-platform-server-1.0.1-SNAPSHOT.jar"
-targetType = "jar"
-
-test_url = "https://bbctest.91dbq.com:8443/"
-uat_url = "https://bbcuat.91dbq.com:8443/"
-prod_url = "https://bbc.91dbq.com:8443/"
-
+def testLinks = [
+  "test": "https://bbctest.91dbq.com:8443/",
+  "uat": "https://bbcuat.91dbq.com:8443/",
+  "prod": "https://bbc.91dbq.com:8443/"
+]
 
 milestone 1
 stage('Dev') {
@@ -25,7 +26,7 @@ stage('Dev') {
                  fingerprintArtifacts: true, 
                  projectName: buildProjectName
             ])
-            if ( needArchive ) {
+            if ( archivePublisher ) {
                 archiveArtifacts artifacts:targetFile, fingerprint: true
             }
             stash name:'targetArchive', includes: targetFile
@@ -63,50 +64,45 @@ milestone 3
 stage('Test') {
     node {
         remote = new Remote(steps, 'test')
-        remote.deploy (playbook, targetFile, BUILD_ID, 'update')
+        remote.deploy (playbook, targetFile, BUILD_ID)
     }
 }
 
 milestone 4
 stage('UAT') {
+    timeout(time:1, unit:'DAYS') {
+        input message: "Test环境 ${testLinks.get('test', '')} 正常了么？可以提交 UAT 了吗?", ok: '准备好了，发布！'
+    }
     lock(resource: "${playbook}-staging-server", inversePrecedence: true) {
-        timeout(time:1, unit:'DAYS') {
-            input message: "Test环境 ${test_url} 正常了么？可以提交 UAT 了吗?", ok: '准备好了，发布！'
-        }
-
         node {
             echo 'UAT deploy start'
             remote = new Remote(steps, 'uat')
-            remote.deploy (playbook, targetFile, BUILD_ID, 'update')
+            remote.deploy (playbook, targetFile, BUILD_ID)
         }
-        
-        timeout(time:1, unit:'DAYS') {
-            input message: " UAT 通过了吗? ${uat_url} ", ok: '通过！'
-        }
+    }
+    timeout(time:1, unit:'DAYS') {
+        input message: " UAT 通过了吗? ${testLinks.get('uat', '')} ", ok: '通过！'
     }
 }
 
 
 milestone 5
 stage ('Production') {
+    timeout(time:1, unit:'DAYS') {
+        input message: "可以提交 Prod 了吗?", ok: '准备好了，发布！'
+    }
     lock(resource: "${playbook}-production-server", inversePrecedence: true) {
-        timeout(time:1, unit:'DAYS') {
-            input message: "可以提交 Prod 了吗?", ok: '准备好了，发布！'
-        }
-        
         node {
             echo 'Production deploy status'
             remote = new Remote(steps, 'prod')
-            remote.deploy (playbook, targetFile, BUILD_ID, 'update')
+            remote.deploy (playbook, targetFile, BUILD_ID)
             echo "Production deployed"
         }
-        
-        timeout(time:1, unit:'DAYS') {
-            input message: "Prod测试完成了吗? ${prod_url} ", ok: '通过！下班，困觉！'
-        }
+    }
+    timeout(time:1, unit:'DAYS') {
+        input message: "Prod测试完成了吗? ${testLinks.get('prod', '')} ", ok: '通过！下班，困觉！'
     }
 }
-
 
 
 
