5a168c30871c9bc106b9665ee2f14fac5f5788e4
Luo Chunhui <wo@luochunhui.com>
2017-02-21 18:16:14 +0800

    remove sonar

--- src/com/jfpal/lib/Utilities.groovy
+++ src/com/jfpal/lib/Utilities.groovy
@@ -26,12 +26,7 @@ class Utilities implements Serializable  {
 
   def qaCheck() {
     steps.parallel( 'quality scan': {
-      node('sonar'){ 
-        withSonarQubeEnv('My SonarQube Server') {
-          // requires SonarQube Scanner for Maven 3.2+
-          sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
-        }
-      }
+        
     },
     'integration tests': {
         
