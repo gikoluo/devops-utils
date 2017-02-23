5f703ec1eb8e21bf3637e163dd3b7bd8543fad32
Luo Chunhui <wo@luochunhui.com>
2017-02-23 12:56:18 +0800

    AutoBuild Boolean

--- src/jenkinsfile/common/Jar.groovy
+++ src/jenkinsfile/common/Jar.groovy
@@ -16,6 +16,8 @@ def buildJob    = env.BUILD_JOB      //Build job in jenkins.
 def targetFile = env.TARGET_FILE     //Build target.
 def autoBuild  = true               //set it to true if you like to build the build job manually.
 def playbook = "";                   //the playbook script file to deploy target, default is "${projectName}/${serviceName}"
+def tags = ["update"]
+
 
 
 if(env.PLAYBOOK) {
@@ -26,8 +28,13 @@ else {
 }
 
 if(env.AUTO_BUILD) {
-    autoBuild = env.AUTO_BUILD
+    autoBuild = env.AUTO_BUILD.toBoolean()
 }
+
+if(env.TAGS) {
+    tags << env.TAGS
+}
+
 
 
 echo "Deploy ${projectName}/${serviceName} with ${playbook}.yml "
@@ -42,7 +49,7 @@ stage('Copy Target') {
         build job: buildJob
     }
     node('master') {
-        utils.copyTarget(buildJob, targetFile)
+        utils.copyTarget(buildJob, targetFile, BUILD_ID, tags)
     }
 }
 
@@ -54,19 +61,19 @@ stage('QA') {
 milestone 3
 stage('Test') {
     remote = new Remote(steps, 'test')
-    remote.deployProcess(playbook, targetFile, BUILD_ID)
+    remote.deployProcess(playbook, targetFile, BUILD_ID, tags)
 }
 
 milestone 4
 stage('UAT') {
     remote = new Remote(steps, 'uat')
-    remote.deployProcess(playbook, targetFile, BUILD_ID)
+    remote.deployProcess(playbook, targetFile, BUILD_ID, tags)
 }
 
 milestone 5
 stage ('Production') {
     remote = new Remote(steps, 'prod')
-    remote.deployProcess(playbook, targetFile, BUILD_ID)
+    remote.deployProcess(playbook, targetFile, BUILD_ID, tags)
 }
 
 
