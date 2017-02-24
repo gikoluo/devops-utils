dffb12326fc25049640a73c1fa0ff6368d9e44fa
Luo Chunhui <wo@luochunhui.com>
2017-02-24 14:49:14 +0800

    notice

--- src/com/jfpal/lib/Noticer.groovy
+++ src/com/jfpal/lib/Noticer.groovy
@@ -13,7 +13,7 @@ class Noticer implements Serializable {
 
     Noticer() {}
 
-    def send(String msg， String type="INFO") {
+    def send(String msg, String type="INFO") {
       script.sh 'bearychat -t "' + type + ': ' + msg + '" -c "DevOps"'
     }
 }
