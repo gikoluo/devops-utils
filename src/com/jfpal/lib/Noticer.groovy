0404d7dadb9225e13761e1bd3cdbbc913921aeb1
Luo Chunhui <wo@luochunhui.com>
2017-03-13 15:53:57 +0800

    mapp

--- src/com/jfpal/lib/Noticer.groovy
+++ src/com/jfpal/lib/Noticer.groovy
@@ -20,7 +20,7 @@ class Noticer implements Serializable {
       def project = playbook.split("/")[0]
 
       steps.node("master") {
-        def fullMsg = "== ${project} / ${inventory} ==\n [${level}]: ${msg}. \n 执行人: ${actor} \n#DevOps #${project} #${inventory}".toString()
+        def fullMsg = "== ${project} / ${inventory} ==\n [${level}]: ${msg}. \n #DevOps #${project} #${inventory}".toString()
         steps.sh "/usr/bin/bearychat -t '${fullMsg}' -c 'DevOps,${project}' -m"
         if (inventory == "prod") {
           steps.sh "/usr/bin/bearychat -t '${fullMsg}' -c '管理组' -m"
