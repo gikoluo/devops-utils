e815177c0a0ea56281875f992d37da06d221fe80
Luo Chunhui <wo@luochunhui.com>
2017-03-13 15:35:40 +0800

    Devops

--- src/jenkinsfile/common/C.groovy
+++ src/jenkinsfile/common/C.groovy
@@ -66,8 +66,8 @@ stage('Copy Target') {
         submitter: 'qa,dev'
     )
 
-    def currentUser = User.current().getFullName();
-    echo "==${currentUser}="
+    //def currentUser = User.current().getFullName();
+    echo "==${userInput}="
 
     targetFile = userInput['modules_list'].readLines().join(",")
     force_restart = userInput['force_restart']
